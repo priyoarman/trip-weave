b3013afc99ac0b2b1f7e395e920ccc46170b74f4
Annamani <annamanik.vit@gmail.com>
2026-06-16 12:50:37 +0200

    duffel services file updated with if else conditions

--- api/src/services/duffel.js
+++ api/src/services/duffel.js
@@ -1,29 +1,32 @@
 import mockFlights from "../data/mock-flights.json" with { type: "json" };
 const USE_MOCK = true;
 export const searchFlights = async (payload) => {
-  if (USE_MOCK) {
-    console.log(" Using MOCK flights data");
-    return mockFlights;
-  }
-
   try {
-    const response = await fetch("https://api.duffel.com/air/offer_requests", {
-      method: "POST",
-      headers: {
-        Authorization: `Bearer ${process.env.DUFFEL_TOKEN}`,
-        "Duffel-Version": "v2",
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    });
+    if (USE_MOCK) {
+      console.log(" Using MOCK flights data");
+      return mockFlights;
+    } else {
+      const response = await fetch(
+        "https://api.duffel.com/air/offer_requests",
+        {
+          method: "POST",
+          headers: {
+            Authorization: `Bearer ${process.env.DUFFEL_TOKEN}`,
+            "Duffel-Version": "v2",
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(payload),
+        },
+      );
 
-    const responseBody = await response.json();
+      const responseBody = await response.json();
 
-    if (!response.ok) {
-      throw new Error(JSON.stringify(responseBody));
+      if (!response.ok) {
+        throw new Error(JSON.stringify(responseBody));
+      }
+
+      return responseBody;
     }
-
-    return responseBody;
   } catch (error) {
     console.log("Duffel failed, using mock Data");
     return mockFlights;
