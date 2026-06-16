fbec1745bb64d420c0b530dd5b4b03cedbbe3e76
Annamani <annamanik.vit@gmail.com>
2026-06-16 13:51:25 +0200

    feat: add ZOD validation for Searching Flights Schema

--- api/src/routers/flights.js
+++ api/src/routers/flights.js
@@ -1,8 +1,8 @@
 import express from "express";
 import { searchFlightsController } from "../controllers/duffel.js";
+import {validate} from "../middleware/validate-data.js";
+import {searchFlightsSchema} from "../schemas/flight-schemas.js";
 
 const router = express.Router();
-
-router.post("/search", searchFlightsController);
-
+router.post("/search", validate(searchFlightsSchema), searchFlightsController,);
 export default router;
