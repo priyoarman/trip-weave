a2ee49d67d27d5446a096d4201d6344359d94e7c
Annamani <annamanik.vit@gmail.com>
2026-06-16 12:46:56 +0200

    removed commented lines from duffel before push changes

--- api/src/services/duffel.js
+++ api/src/services/duffel.js
@@ -1,32 +1,4 @@
-// import mockFlights from "../data/mock-flights.json" assert { type: "json" };
-// export const searchFlights = async (payload) => {
-//   try {
-//     const response = await fetch("https://api.duffel.com/air/offer_requests", {
-//       method: "POST",
-//       headers: {
-//         Authorization: `Bearer ${process.env.DUFFEL_TOKEN}`,
-//         "Duffel-Version": "v2",
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify(payload),
-//     });
-
-//     const responseBody = await response.json();
-//     if (!response.ok) {
-//      throw new Error(JSON.stringify(responseBody));
-//     }
-//     return responseBody;
-//   } catch (error) {
-//     console.warn(
-//       "Duffel unavailable, serving mock flight data:",
-//       error.message,
-//     );
-
-//     return mockFlights;
-//   }
-// };
 import mockFlights from "../data/mock-flights.json" with { type: "json" };
-
 const USE_MOCK = true;
 export const searchFlights = async (payload) => {
   if (USE_MOCK) {
@@ -53,7 +25,7 @@ export const searchFlights = async (payload) => {
 
     return responseBody;
   } catch (error) {
-    console.log("Duffel failed, using mock fallback");
+    console.log("Duffel failed, using mock Data");
     return mockFlights;
   }
 };
